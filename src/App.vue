516fc03d4df9c0997d4812d1707e5c73571e45f0
joerivanarkel <Joeri@vanarkel.net>
2024-01-02 22:10:40 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import MenuBar from './components/MenuBar.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <MenuBar />
+    <div class="content">
+      <h1>Vue 3 + Vite</h1>
+      <p>Click on the Vite and Vue logos to learn more</p>
+      <HelloWorld :msg="'Hello World'" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+.app-container {
+  display: flex;
+}
+
+
+</style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="menu">
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<style lang="scss">
+@import '../styles/variables/_colors.scss';
+
+.menu {
+  flex: 2;
+  padding-left: 3rem;
+  padding-right: 3rem;
+}
+
+nav {
+  ul {
+    list-style: none;
+    padding: 0;
+    display: block;
+    li {
+      a {
+        color: $light-color;
+        text-decoration: none;
+        font-size: 1em;
+        font-weight: 500;
+        transition: color 0.3s ease;
+        &:hover {
+          color: $primary-color;
+        }
+      }
+    }
+  }
+}
+</style>
