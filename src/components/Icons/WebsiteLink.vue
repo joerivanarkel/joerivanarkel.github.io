3f69b804faf9aa6f3aa8bd0edc9a76a812c8bd06
Joeri van Arkel <98022003+joerivanarkel@users.noreply.github.com>
2024-01-03 09:29:32 +0000

    change logo color

--- src/components/Icons/WebsiteLink.vue
+++ src/components/Icons/WebsiteLink.vue
@@ -1,16 +1,36 @@
 <template>
     <a :href="link" target="_blank" rel="noopener noreferrer">
-        <img src="../../assets/website-logo-white.svg" alt="Website Link" />
+        <img :src="websiteLogo" alt="Website Link" />
     </a>
 </template>
 
 <script lang="ts">
+import websiteLogoWhite from '../../assets/website-logo-white.svg';
+import websiteLogoBlack from '../../assets/website-logo.svg';
+
 export default {
     name: 'WebsiteLink',
     props: {
         link: {
             type: String,
             required: true
+        }
+    },
+    data() {
+        return {
+            prefersColorScheme: 'dark'
+        }
+    },
+    mounted() {
+        this.prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+            this.prefersColorScheme = e.matches ? 'dark' : 'light';
+        });
+    },
+
+    computed: {
+        websiteLogo() {
+            return this.prefersColorScheme === 'dark' ? websiteLogoWhite : websiteLogoBlack;
         }
     }
 }
